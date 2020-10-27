082fb291488ecc9f0d82608eec36cb80578930d7
perorin <perorin-toyoko@outlook.jp>
2020-10-27 21:48:08 +0900

    コミット支援ツール作成中

--- commit_helper.sh
+++ commit_helper.sh
@@ -12,87 +12,153 @@ if [ -e $git_dir"\.git" ]; then
     ok "Gitディレクトリであることを確認。"
 else
     ng "Gitディレクトリではありません。"
-    exit 1
+    errorEnd
 fi
+#
+#echo
+#echo "########################################"
+#echo "# ブランチの確認"
+#echo "########################################"
+#logExec 'git symbolic-ref --short HEAD'
+#branchName=`git symbolic-ref --short HEAD`
+#echo
+#echo "あなたが見ているブランチは [ $branchName ] です。"
+#echo "あなたは [ $branchName ] にコミットしようとしています。"
+#yn=""; read -r -p "間違いはありませんか？ (Y/N): " yn
+#case "$yn" in [yY]*) ;; *) errorEnd ;; esac
+#
+#echo
+#echo "########################################"
+#echo "# コミット対象の確認"
+#echo "########################################"
+#repDif=`git log origin/$branchName..$branchName`
+#gitSts=`git status -s -uall`
+#if [ -z "${repDif}${gitSts}" ]; then
+#    ng "ワークツリーはクリーンです。"
+#    ng "コミットすべき対象がありません。"
+#    errorEnd
+#else
+#    echo "以下のファイルからコミット対象を手動でバックアップしてください。"
+#    logExec 'git status -sb -uall'
+#    echo "M_：Staged  _M：Unstaged  ??：Untracked"
+#    echo
+#    git status -s -uall
+#    echo
+#    yn=""; read -r -p "バックアップしたらEnterキーを押下：" yn
+#fi
+#
+#echo
+#echo "########################################"
+#echo "# ワークツリーの状態確認"
+#echo "########################################"
+#repDif=`git log origin/$branchName..$branchName`
+#gitSts=`git status -s`
+#if [ -z "${repDif}${gitSts}" ]; then
+#    ng "ワークツリーはクリーンです。"
+#    ng "コミットすべき対象がありません。"
+#    errorEnd
+#else
+#    ok "ワークツリーに変更されているファイルがあることを確認"
+#    echo
+#    echo "＜ワークツリーの状態を出力＞"
+#    logExec 'git status -uall'
+#    git status -uall
+#    echo
+#    echo "変更しているファイルを一旦、全て初期化します。"
+#    yn=""; read -r -p "よろしいですか？ (Y/N): " yn
+#    case "$yn" in [yY]*) ;; *) errorEnd ;; esac
+#
+#    echo
+#    echo "＜ワークツリーを初期化＞"
+#    logExec 'git reset --hard HEAD'
+#    git reset --hard HEAD
+#    echo
+#    logExec 'git clean -df'
+#    git clean -df
+#fi
+#
+#echo
+#echo "########################################"
+#echo "# ローカルリポジトリとリモートリポジトリの状態比較"
+#echo "########################################"
+#repDif=`git log origin/$branchName..$branchName`
+#if [ ! -z "$repDif" ]; then
+#    warn "プッシュしていないコミットがあります。"
+#    echo
+#    echo "＜プッシュされていないコミットを表示＞"
+#    logExec "git log origin/$branchName..$branchName"
+#    git log origin/$branchName..$branchName
+#    echo
+#    echo "プッシュされていないコミットを全て削除します。"
+#    yn=""; read -r -p "よろしいですか？ (Y/N): " yn
+#    case "$yn" in [yY]*) ;; *) errorEnd ;; esac
+#
+#    echo
+#    echo "＜プッシュされていないコミットを全て削除＞"
+#    logExec 'git reset --hard origin/HEAD'
+#    git reset --hard origin/HEAD
+#else
+#    ok "プッシュしていないコミットが無いことを確認"
+#fi
+#
+#echo
+#echo "########################################"
+#echo "# コミット対象ファイルのステージング"
+#echo "########################################"
+#echo "手動でバックアップしたコミット対象ファイルを格納してください。"
+#yn=""; read -r -p "格納したらEnterキーを押下：" yn
+#gitSts=`git status -s`
+#if [ -z "$gitSts" ]; then
+#    ng "ワークツリーはクリーンです。"
+#    ng "ステージングすべき対象がありません。"
+#    errorEnd
+#else
+#    ok "ワークツリーに変更されているファイルがあることを確認"
+#    echo
+#    echo "コミット対象は以下のファイルです。"
+#    logExec 'git status -sb -uall'
+#    echo "M_：Staged  _M：Unstaged  ??：Untracked"
+#    echo
+#    git status -s -uall
+#    echo
+#    yn=""; read -r -p "間違いないですか？ (Y/N): " yn
+#    case "$yn" in [yY]*) ;; *) errorEnd ;; esac
+#    echo "ステージングします。"
+#    logExec 'git add -A'
+#    git add -A
+#fi
 
 echo
 echo "########################################"
-echo "# ブランチの確認"
+echo "# リモートリポジトリの修正をダウンロード"
 echo "########################################"
-logExec 'git symbolic-ref --short HEAD'
-branchName=`git symbolic-ref --short HEAD`
-echo
-echo "あなたが見ているブランチは「$branchName」です。"
-echo "あなたは「$branchName」にコミットしようとしています。"
-yn=""; read -r -p "間違いはありませんか？ (Y/N): " yn
-case "$yn" in [yY]*) ;; *) echo ; exit 1 ;; esac
+logExec 'git fetch'
+git fetch
 
 echo
 echo "########################################"
-echo "# コミット対象の確認"
+echo "# リモートリポジトリの修正をローカルリポジトリに取り込み"
 echo "########################################"
-gitSts=`git status -s`
-if [ -z "$gitSts" ]; then
-    ng "ワークツリーはクリーンです。"
-    ng "コミットすべき対象がありません。"
-    exit 1
-else
-    echo "以下のファイルorフォルダーからコミット対象を手動でバックアップしてください。"
-    logExec 'git status -s'
-    git status -s
-fi
-
-
-
+logExec 'git merge --ff-only FETCH_HEAD'
+mergeFetch=`git merge --ff-only FETCH_HEAD`
+git merge --ff-only FETCH_HEAD
 
 
 echo
 echo "########################################"
-echo "# ワークツリーの状態確認"
+echo "# コミット対象ファイルのコミット"
 echo "########################################"
-gitSts=`git status -s`
-if [ -z "$gitSts" ]; then
-    ng "ワークツリーはクリーンです。"
-    ng "コミットすべき対象がありません。"
-    exit 1
-else
-    ok "ワークツリーはクリーンではありません。"
-    echo
-    echo "＜ワークツリーの状態を出力＞"
-    logExec 'git status'
-    git status
-    echo
-    echo "変更しているファイルを全て初期化します。"
-    yn=""; read -r -p "よろしいですか？ (Y/N): " yn
-    case "$yn" in [yY]*) ;; *) echo ; exit 1 ;; esac
+while :
+do
+    comment=""; read -r -p "コミットコメントを入力してください: " comment
+    echo "**************************************"
+    echo "$comment"
+    echo "**************************************"
+    yn=""; read -r -p "コミットコメントに間違いはないですか？ (Y/N): " yn
+    case "$yn" in [yY]*) break ;; *) continue ;; esac
+done
+echo "コミットします。"
+logExec "git commit -m $comment"
+git commit -m $comment
 
-    echo
-    echo "＜ワークツリーを初期化＞"
-    logExec 'git reset --hard HEAD'
-    git reset --hard HEAD
-    echo
-    logExec 'git clean -df'
-    git clean -df
-fi
 
-echo
-echo "########################################"
-echo "# ローカルリポジトリとリモートリポジトリの状態比較"
-echo "########################################"
-repDif=`git log origin/$branchName..$branchName`
-if [ ! -z "$repDif" ]; then
-    warn "プッシュしていないコミットがあります。"
-    echo
-    echo "＜プッシュされていないコミットを表示＞"
-    logExec "git log origin/$branchName..$branchName"
-    git log origin/$branchName..$branchName
-    echo
-    echo "プッシュされていないコミットを全て削除します。"
-    yn=""; read -r -p "よろしいですか？ (Y/N): " yn
-    case "$yn" in [yY]*) ;; *) echo ; exit 1 ;; esac
-
-    echo
-    echo "＜プッシュされていないコミットを全て削除＞"
-    logExec 'git reset --hard origin/HEAD'
-    git reset --hard origin/HEAD
-fi
